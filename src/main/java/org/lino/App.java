f7214efaf16fe1209b59d626a8d9dd4b1f81b6d0
Lino Chandran <linochandran@gmail.com>
2022-02-02 18:46:12 -0500

    updated code

--- src/main/java/org/lino/App.java
+++ src/main/java/org/lino/App.java
@@ -1,13 +1,18 @@
 package org.lino;
 
+import org.lino.model.Person;
+import org.lino.service.PersonService;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        Person person=new Person("Lino", "Chandran", "linochandran@gmail.com");
+        PersonService service=new PersonService();
+        service.save(person);
     }
 }
