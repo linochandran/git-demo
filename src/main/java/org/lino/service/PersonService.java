4e7e9dc08b8e8a1f2bfb0877ef893f377cad9de6
Lino Chandran <linochandran@gmail.com>
2022-02-02 20:55:46 -0500

    updated code

--- src/main/java/org/lino/service/PersonService.java
+++ src/main/java/org/lino/service/PersonService.java
@@ -6,7 +6,9 @@ public class PersonService {
 
     public void save(Person person){
         if(person!=null){
-            System.out.println(person);
+            System.out.println("First Name : "+person.getFirstName());
+            System.out.println("Last Name  : "+person.getFirstName());
+            System.out.println("Email Id   : "+person.getFirstName());
         }else {
             System.out.println("Invalid Data to process");
         }
